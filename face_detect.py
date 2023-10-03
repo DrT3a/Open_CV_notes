6d34d250da2eb75366595445e452542e6b9b49b5
DrT3a <drostavlarios@hotmail.com>
2023-10-03 11:46:37 +0300

    Added Faces

---
+++ face_detect.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+
+# Import image with a face
+# img = cv.imread('E:\Drs\Programming\Open_CV_tutorial\Images\woman_face.jpg')
+# cv.imshow('Original', img)
+# cv.waitKey(0)
+
+# Import image with five faces
+img = cv.imread('E:\Drs\Programming\Open_CV_tutorial\Images\group_of_5.jpg')
+cv.imshow('Group', img)
+cv.waitKey(0)
+
+# Convert to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+cv.waitKey(0)
+
+# Read the haar_face.xml file. Set it in a variable and pass the path.
+# The haar_face.xml was obtained from OpenCV github.
+haar_cascade = cv.CascadeClassifier('haar_face.xml') # Reads all 33k lines of code in haar_face.xml and stores them in the haar_cascade variable.
+
+# Detect the face in the image.
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=11) # minNeighbors specifies the number of neighbors a rectangle should have to be called a face.
+# The detectMultiScale takes the gray img, uses the variables scaleFactor and minNeighbors, to detect a face and return the rectangular coordinates of that face as a LIST to faces_rect.
+
+# We can print the number of rectangles using len()
+print(f'Number of faces found = {len(faces_rect)}')
+
+# Since faces_rect are the rectangular coordinates for the faces that are present in the img we can loop over this list and grab the coordinates of those images and draw a rectangle over the detected faces.
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2) # point1:(x,y), point2:(x+w,y+h)
+
+cv.imshow('Detected Faces', img)
+cv.waitKey(0)
+
+# We see that it detects 8 faces instead of 5 (using scaleFactor=1.1, minNeighbors=3). That is because hardcascades are really sensitive to noise. If we want to minimise noise sensitivity is to tweak scaleFactor and min Neighbors.
+# Testing the values we find using minNeighbors=11 finds the correct amount of faces.
+
+# Haar cascades is not for advanced projects but is easy to use. It can also be used for video, where the whole thing applies on a frame by frame basis.
---
+++ gradients.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import numpy as np
+
+# Gradients and Edge detection are completely different from a mathematical point of view, but programmatically you can get away thinking of them as similar. We have already used Canny edge detection that is a multistep method. Here we discuss two gradient methods.
+img = cv.imread('E:\Drs\Programming\Open_CV_tutorial\Images\yen.jpg')
+cv.imshow('Original', img)
+cv.waitKey(0)
+
+#__________________Laplacian method____________________#
+
+# Convert the original image to grayscale.
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Grayscale', gray)
+cv.waitKey(0)
+
+# Define variable for Laplacian transformation, takes in a source img and applies datadepth
+lap = cv.Laplacian(gray, cv.CV_64F) # Computes two gradiens of the grayscale img.
+# Images cannot have negative pixel values so we compute the absolute value of that img * all of the pixels of that img are converted to their absolute values* and then we convert that to uint8:image specific datatype.
+lap = np.uint8(np.absolute(lap))
+cv.imshow('Laplacian', lap)
+cv.waitKey(0)
+
+#_________________Sobel Gradient Magnitude Representation____________#
+# Sobel computes gradients on two directions the X and Y.
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+cv.imshow('Sobelx', sobelx)
+cv.imshow('Sobely', sobely)
+cv.waitKey(0)
+
+# We can also get the combined sobel img by combining sobelx adn sobely. We combine using bitwise_or (Ένωση Venn)
+combined_sobel = cv.bitwise_or(sobelx, sobely)
+cv.imshow('Combined Sobel', combined_sobel)
+cv.waitKey(0)
+
+# Lets compare to Canny edge detection.
+canny = cv.Canny(gray, 150, 175)
+cv.imshow('Canny', canny)
+cv.waitKey(0)
